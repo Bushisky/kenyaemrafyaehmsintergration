53026fc354ca7b2cf0da70b694c90d2cdf237e7a
ningosi <nicotwendelee@gmail.com>
2020-02-14 12:10:42 +0300

    Genearte the patient identifiers on 2 separate categories

--- api/src/main/java/org/openmrs/module/kenyaemrafyaehmsintergration/KenyaEMRAfyaehmsIntergrationActivator.java
+++ api/src/main/java/org/openmrs/module/kenyaemrafyaehmsintergration/KenyaEMRAfyaehmsIntergrationActivator.java
@@ -187,22 +187,35 @@ public class KenyaEMRAfyaehmsIntergrationActivator extends BaseModuleActivator {
 		}
 	}
 	
-	protected PatientIdentifier generatePatientIdentifier() {
+	protected PatientIdentifier generatePatientIdentifier(String type) {
 		IdentifierSourceService iss = Context.getService(IdentifierSourceService.class);
 		IdentifierSource idSource = iss.getIdentifierSource(1); // this is the default OpenMRS identifier source
 		PatientService patientService = Context.getPatientService();
 		
 		UUID uuid = UUID.randomUUID();
-		
-		PatientIdentifierType patientIdentifierType = patientService
-		        .getPatientIdentifierTypeByUuid("dfacd928-0370-4315-99d7-6ec1c9f7ae76");
-		
 		PatientIdentifier pid = new PatientIdentifier();
-		pid.setIdentifierType(patientIdentifierType);
-		String identifier = iss.generateIdentifier(idSource, "New OpenMRS ID with CheckDigit");
-		pid.setIdentifier(identifier);
-		pid.setPreferred(true);
-		pid.setUuid(String.valueOf(uuid));
+		
+		PatientIdentifierType patientIdentifierType_old = patientService
+		        .getPatientIdentifierTypeByUuid("dfacd928-0370-4315-99d7-6ec1c9f7ae76");
+		PatientIdentifierType patientIdentifierType_new = patientService
+		        .getPatientIdentifierTypeByUuid("05a29f94-c0ed-11e2-94be-8c13b969e334");
+		
+		if (type.equals("old")) {
+			pid.setIdentifierType(patientIdentifierType_old);
+			String identifier = Context.getService(IdentifierSourceService.class).generateIdentifier(
+			    patientIdentifierType_old, "Registration");
+			pid.setIdentifier(identifier);
+			pid.setLocation(Context.getLocationService().getLocation(1));
+			pid.setPreferred(true);
+			pid.setUuid(String.valueOf(uuid));
+		} else if (type.equals("new")) {
+			pid.setIdentifierType(patientIdentifierType_new);
+			String identifier = iss.generateIdentifier(idSource, "New OpenMRS ID with CheckDigit");
+			pid.setIdentifier(identifier);
+			pid.setLocation(Context.getLocationService().getLocation(1));
+			pid.setPreferred(true);
+			pid.setUuid(String.valueOf(uuid));
+		}
 		
 		return pid;
 		
@@ -216,21 +229,26 @@ public class KenyaEMRAfyaehmsIntergrationActivator extends BaseModuleActivator {
 		PatientService patientService = Context.getPatientService();
 		AdministrationService as = Context.getAdministrationService();
 		
-		List<List<Object>> patientIds = as
+		List<List<Object>> patientIds_old = as
 		        .executeSQL(
 		            "SELECT patient_id FROM patient_identifier WHERE patient_id NOT IN (SELECT patient_id FROM patient_identifier p INNER JOIN patient_identifier_type pt ON (p.identifier_type = pt.patient_identifier_type_id AND pt.uuid = 'dfacd928-0370-4315-99d7-6ec1c9f7ae76'))",
 		            true);
 		
-		if (patientIds.size() == 0) {
+		List<List<Object>> patientIds_new = as
+		        .executeSQL(
+		            "SELECT patient_id FROM patient_identifier WHERE patient_id NOT IN (SELECT patient_id FROM patient_identifier p INNER JOIN patient_identifier_type pt ON (p.identifier_type = pt.patient_identifier_type_id AND pt.uuid = '05a29f94-c0ed-11e2-94be-8c13b969e334'))",
+		            true);
+		
+		if (patientIds_old.size() == 0 || patientIds_new.size() == 0) {
 			// no patients to process
 			return;
 		}
 		// get the identifier source copied from RegistrationCoreServiceImpl
 		
-		for (List<Object> row : patientIds) {
+		for (List<Object> row : patientIds_old) {
 			Patient p = patientService.getPatient((Integer) row.get(0));
 			// Create new Patient Identifier
-			PatientIdentifier pid = generatePatientIdentifier();
+			PatientIdentifier pid = generatePatientIdentifier("old");
 			pid.setPatient(p);
 			try {
 				log.info("Adding OpenMRS ID " + pid.getIdentifier() + " to patient with id " + p.getPatientId());
@@ -242,7 +260,24 @@ public class KenyaEMRAfyaehmsIntergrationActivator extends BaseModuleActivator {
 				log.error("Error updating OpenMRS identifier for patient #" + p.getPatientId(), e);
 			}
 		}
-		log.info("All patients updated with  OpenMRS ID");
+		log.info("All patients updated with  OLD OpenMRS ID");
+		
+		for (List<Object> row : patientIds_new) {
+			Patient p = patientService.getPatient((Integer) row.get(0));
+			// Create new Patient Identifier
+			PatientIdentifier pid = generatePatientIdentifier("new");
+			pid.setPatient(p);
+			try {
+				log.info("Adding new OpenMRS ID " + pid.getIdentifier() + " to patient with id " + p.getPatientId());
+				// Save the patient Identifier
+				patientService.savePatientIdentifier(pid);
+			}
+			catch (Exception e) {
+				// log the error to the alert service but do not rethrow the exception since the module has to start
+				log.error("Error updating OpenMRS identifier for patient #" + p.getPatientId(), e);
+			}
+		}
+		log.info("All patients updated with  NEW OpenMRS ID");
 	}
 	
 }
